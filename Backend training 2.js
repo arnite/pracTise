4ca187244a4a635f3bef37e393ad01069977ad33
arnite <udembao429@gmail.com>
2025-02-12 13:19:31 +0100

    day 3

---
+++ Backend training 2.js
@@ -0,0 +1,54 @@
+//Filtering, sorting and pagination
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    //Filtering
+    const queryObj = { ...queryString };
+    const excludedFields = ["page", "fields", "sort", "limit"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(lt|gt|lte|gte)\b/g, (match) => `$${match}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("createdAt");
+    }
+
+    return this;
+  }
+
+  limitField() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v");
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page || 1;
+    const limit = this.queryString.limit || 10;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
