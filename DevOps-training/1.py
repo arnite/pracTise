9e5719fce9a3f09f74af9fb8bf9212183634bcea
arnite <udembao429@gmail.com>
2025-10-16 09:41:22 +0100

    another update

---
+++ DevOps-training/1.py
@@ -0,0 +1,46 @@
+# name = "arnold"
+# age = 45
+
+# print(f"Name: {name}, Age: {age}")
+
+# a = 5
+# b = 8
+
+# print(f"Sum: {a + b}, Difference: {a - b}, Product: {a * b}")
+
+# firstName = "Ogechukwu"
+# lastname = "Udemba"
+
+# 1. Temperature Converter**
+# Write a program that asks the user for a temperature in Celsius and converts it to Fahrenheit using the formula:
+# ```
+# F = (C * 9/5) + 32
+# ```
+
+# celsius = float(input("Type temperature in celsius to be converted to farenheit:"))
+
+# farenheit = (celsius * 9/5) + 32
+
+# print(f"Celsius: {celsius} = Farenheit: {farenheit}")
+
+
+
+# 2. Even or Odd Checker**
+# Ask the user for an integer and determine whether it’s even or odd.
+
+# integer = float(input("Type number you want to know if it is even or odd: "))
+
+# if integer % 2 == 0:
+#     print(f"Even number")
+# else:
+#     print(f'Odd number')
+
+
+
+# Take a word from the user and print it reversed.
+
+word = input("Write word to reverse: ")
+
+reversedWord = word[::-1]
+
+print(f"reversed: {reversedWord}")
